29f3ac911cdbf3a02f82ef8a5818fb0428307ad1
J0SH1X <aljoshua.hell@gmail.com>
2019-01-23 01:08:38 +0100

    leo: Rebrand to Lineage

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -16,7 +16,7 @@ $(call inherit-product, device/htc/leo/device_leo.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := leo
-PRODUCT_NAME := cm_leo
+PRODUCT_NAME := lineage_leo
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := Htc Hd2
 PRODUCT_MANUFACTURER := HTC
